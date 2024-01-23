f47aea1d043e0e17cf7571e6feca15fdb6eaef6a
RisanStar <nBuckley2023@elam.co.uk>
2024-01-23 12:46:47 +0000

    Started on use mechanic

--- Assets/Scripts/PlayerMechanics.cs
+++ Assets/Scripts/PlayerMechanics.cs
@@ -8,29 +8,43 @@ public class PlayerMechanics : MonoBehaviour
 
 {
     private bool pickUpAllowed;
-    public float pickUpTimer;
-    private float pickUpCountdown = 0f;
+    public float Timer;
+    private float Count = 0f;
 
-    private bool canUse;
+    private bool canUseKey;
+    private bool keyAllowed;
 
+    private float doorSpeed;
+    private int pointsIndex;
+    [SerializeField] Transform[] Points;
 
     void Start()
     {
-        pickUpCountdown = pickUpTimer;
+        Count = Timer;
+        transform.position = Points[pointsIndex].transform.position;
     }
     //PICKUP WITH E WITH TIMER
     private void Update()
     {
         if (pickUpAllowed && Input.GetKey(KeyCode.E))
         {
-            pickUpCountdown -= 1f * Time.deltaTime;
+            Count -= 1f * Time.deltaTime;
         }
             
-        if (pickUpCountdown <= 0f) { pickUpCountdown = 0f;}
-        if (pickUpCountdown == 0f)
+        if (Count <= 0f) { Count = 0f;}
+        if (Count == 0f && (pickUpAllowed))
         {
             Pickup();
-            canUse = true;
+            canUseKey = true;
+        }
+        
+        if (keyAllowed && Input.GetKey(KeyCode.E))
+        {
+            Count -= 1f * Time.deltaTime;
+        }
+        if (Count == 0f && (keyAllowed))
+        {
+            
         }
                
     }
@@ -52,6 +66,21 @@ public class PlayerMechanics : MonoBehaviour
         }
     }
 
+    private void OnTriggerEnter2D(Collider collision)
+    {
+        if (collision.CompareTag("Door") && (canUseKey))
+        {
+            keyAllowed = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider collision)
+    {
+        if (collision.CompareTag("Door") && (canUseKey))
+        {
+            keyAllowed = false;
+        }
+    }
     //PICKUP REFERENCE
     private void Pickup()
     {
