fd829dc95c08cb822d08aa357a4572c6479ca051
RisanStar <nBuckley2023@elam.co.uk>
2024-01-26 12:01:59 +0000

    level 1

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,8 +9,8 @@ public class PlayerMovement : MonoBehaviour
     private float vertical;
     private float horizontal;
 
-    private float climbingSpeed = 3f;
-    private float speed = 4f;
+    public float climbingSpeed = 3f;
+    public float speed = 4f;
     private float speedMulti = 1.5f;
     private float airSpeedMulti = 1.3f;
     private float climpingSpeedMulti = 1.2f;
@@ -33,32 +33,35 @@ public class PlayerMovement : MonoBehaviour
     private bool isFastClimbing;
     private bool isFacingRight = true;
 
+    private GameObject currentOneWayPlatform;
+    private GameObject currentOneWayLadder;
+
     //REFERENCES
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
-
+    [SerializeField] private BoxCollider2D playerCollider;
     private void Update()
     {
         //CALCULATING JUMPING INPUT
         horizontal = Input.GetAxisRaw("Horizontal");
         vertical = Input.GetAxisRaw("Vertical");
-        if (Input.GetButtonDown("Jump") && IsGrounded() && !isClimbing)
+        if(Input.GetButtonDown("Jump") && IsGrounded() && !isClimbing)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
         }
-        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f && !isClimbing)
+        if(Input.GetButtonUp("Jump") && rb.velocity.y > 0f && !isClimbing)
         {
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * .5f);
         }
-        if (rb.velocity.y < 0f && !isClimbing)
+        if(rb.velocity.y < 0f && !isClimbing)
         {
             rb.velocity += Vector2.up * Physics2D.gravity.y * (FallMultiplier - 1) * Time.deltaTime;
         }
         Flip();
 
         //CALCULATING CLIMBING INPUT
-        if (isLadder && Mathf.Abs(vertical) > 0f)
+        if(isLadder && Mathf.Abs(vertical) > 0f)
         {
             isClimbing = true;
         }
@@ -72,7 +75,7 @@ public class PlayerMovement : MonoBehaviour
         }
 
         //CALCULATING RUNNING INPUT
-        if (Input.GetKey(KeyCode.LeftShift))
+        if(Input.GetKey(KeyCode.LeftShift))
         {
             isRunning = true;
         }
@@ -82,7 +85,7 @@ public class PlayerMovement : MonoBehaviour
         }
 
         //STAMINA
-        if ((isRunning && !isFastClimbing))
+        if((isRunning && !isFastClimbing))
         {
             stamina -= runCost * Time.deltaTime;
             if (recharge != null) StopCoroutine(recharge);
@@ -98,6 +101,22 @@ public class PlayerMovement : MonoBehaviour
         {
             isTired = false;
         }
+
+        if(Input.GetKeyDown(KeyCode.S))
+        {
+            if (currentOneWayPlatform != null)
+            {
+                StartCoroutine(DisableCollisionPlatform());
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.D))
+        {
+            if (currentOneWayPlatform != null)
+            {
+                StartCoroutine(DisableCollisionLadder());
+            }
+        }
     }
 
 
@@ -107,7 +126,7 @@ public class PlayerMovement : MonoBehaviour
         rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
 
         //CLIMBING
-        if (isClimbing)
+        if(isClimbing)
         {
             rb.gravityScale = 0f;
             rb.velocity = new Vector2(rb.velocity.x, vertical * climbingSpeed);
@@ -117,32 +136,32 @@ public class PlayerMovement : MonoBehaviour
             rb.gravityScale = 2f;
         }
 
-        if (isFastClimbing)
+        if(isFastClimbing)
         {
             rb.velocity = new Vector2(rb.velocity.x, vertical * climbingSpeed * climpingSpeedMulti);
         }
 
         //RUNNING SPEED  
-        if ((isRunning) && IsGrounded())
+        if((isRunning) && IsGrounded())
         {
             rb.velocity = new Vector2(horizontal * speed * speedMulti, rb.velocity.y);
         }
-        if ((isRunning) && !IsGrounded())
+        if((isRunning) && !IsGrounded())
         {
             rb.velocity = new Vector2(horizontal * speed * airSpeedMulti, rb.velocity.y);
         }
 
         //STAMINA 
-        if (isTired)
+        if(isTired)
         {
             rb.velocity = new Vector2(horizontal * speed * tiredMulti, rb.velocity.y);
         }
     }
-    
+
     //DIRECTION
     private void Flip()
     {
-        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
+        if(isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
         {
             isFacingRight = !isFacingRight;
             Vector3 localScale = transform.localScale;
@@ -160,7 +179,7 @@ public class PlayerMovement : MonoBehaviour
     //CLIMBING COLLISIONS
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Ladder"))
+        if(collision.CompareTag("Ladder"))
         {
             isLadder = true;
         }
@@ -168,24 +187,74 @@ public class PlayerMovement : MonoBehaviour
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.CompareTag("Ladder"))
+        if(collision.CompareTag("Ladder"))
         {
             isLadder = false;
             isClimbing = false;
         }
     }
 
-    //STAMINA RECHARGE
-    private IEnumerator rechargeStamina()
+
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        yield return new WaitForSeconds(1f);
-        while (stamina < maxStamina && (!isRunning))
+        if(collision.gameObject.CompareTag("OneWay"))
         {
-            stamina += chargeRate / 10f;
-            if (stamina > maxStamina) stamina = maxStamina;
-            //stamina fill amount should go here
-            yield return new WaitForSeconds(.1f);
+            currentOneWayPlatform = collision.gameObject;
+        }
+        if(collision.gameObject.CompareTag("OneWayLadder"))
+        {
+            currentOneWayLadder = collision.gameObject;
         }
     }
 
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("OneWay"))
+        {
+            currentOneWayPlatform = null;
+        }
+
+        if(collision.gameObject.CompareTag("OneWayLadder"))
+        {
+            currentOneWayLadder = null;
+        }
+
+
+    }
+
+
+
+
+
+    //STAMINA RECHARGE
+    private IEnumerator rechargeStamina()
+        {
+            yield return new WaitForSeconds(1f);
+            while (stamina < maxStamina && (!isRunning))
+            {
+                stamina += chargeRate / 10f;
+                if (stamina > maxStamina) stamina = maxStamina;
+                //stamina fill amount should go here
+                yield return new WaitForSeconds(.1f);
+            }
+        }
+
+        private IEnumerator DisableCollisionPlatform()
+        {
+            BoxCollider2D platformCollider = currentOneWayPlatform.GetComponent<BoxCollider2D>();
+            Physics2D.IgnoreCollision(playerCollider, platformCollider);
+            yield return new WaitForSeconds(0.25f);
+            Physics2D.IgnoreCollision(playerCollider, platformCollider, false);
+        }
+
+    private IEnumerator DisableCollisionLadder()
+    {
+        BoxCollider2D platformCollider = currentOneWayLadder.GetComponent<BoxCollider2D>();
+        Physics2D.IgnoreCollision(playerCollider, platformCollider);
+        yield return new WaitForSeconds(0.25f);
+        Physics2D.IgnoreCollision(playerCollider, platformCollider, false);
+    }
 }
+
+    
