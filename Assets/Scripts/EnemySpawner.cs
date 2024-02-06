476d83187ee9ab2a67da0655128a2eefcde1cc5a
RisanStar <nBuckley2023@elam.co.uk>
2024-02-06 12:47:24 +0000

    Enemy

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using Unity.VisualScripting;
 using UnityEngine;
+using Pathfinding;
 using static UnityEditor.ShaderGraph.Internal.KeywordDependentCollection;
 
 public class EnemySpawner : MonoBehaviour
