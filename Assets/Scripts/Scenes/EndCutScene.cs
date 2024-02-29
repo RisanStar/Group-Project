770a6717181331125a77c25b645683aa73190abb
RisanStar <nBuckley2023@elam.co.uk>
2024-02-29 15:40:18 +0000

    Transition

--- Assets/Scripts/Scenes/EndCutScene.cs
+++ Assets/Scripts/Scenes/EndCutScene.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class EndCutScene : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] public Animator fadeIn;
+    [SerializeField] public Animator noahRise;
+    [SerializeField] public Animator noahTalking;
+    [SerializeField] public Animator textBox;
+    public float transitionTimer;
+    private float count;
+    private float talkCount;
+    public float talkTimer;
+ 
+
+    private bool canTalk = false;
+
+    private void Start()
     {
-        
+        fadeIn.SetTrigger("Start");
+        count = transitionTimer;
+        talkCount = talkTimer;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        NoahRise();
+        if (canTalk)
+        {
+            talkCount -= 1f * Time.deltaTime;
+            if (talkCount <= 0f) {talkCount = 0;}
+            if (talkCount == 0)
+            {
+                noahTalking.SetBool("NoahTalk", true);
+                textBox.SetBool("TextOn", true);
+            }
+        }
     }
+
+    private void NoahRise()
+    {
+        count -= 1f * Time.deltaTime;
+        if (count <= 0) { count = 0;}
+        if (count == 0)
+        {
+            noahRise.SetBool("NoahOn",true);
+            canTalk = true;
+        }
+    }
+
 }
