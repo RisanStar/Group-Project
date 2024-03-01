e7e4350afba1af9fd47d99eef1452d7bf567379c
RisanStar <nBuckley2023@elam.co.uk>
2024-03-01 10:42:53 +0000

    EndCutscene - Nora

--- Assets/Scripts/Scenes/EndCutScene.cs
+++ Assets/Scripts/Scenes/EndCutScene.cs
@@ -5,35 +5,67 @@ using UnityEngine;
 public class EndCutScene : MonoBehaviour
 {
     [SerializeField] public Animator fadeIn;
+
     [SerializeField] public Animator noahRise;
     [SerializeField] public Animator noahTalking;
-    [SerializeField] public Animator textBox;
+    [SerializeField] public Animator noahName;
+    [SerializeField] public Animator noahTextBox;
+
+    [SerializeField] public Animator noraRise;
+    [SerializeField] public Animator noraTalking;
+    [SerializeField] public Animator noraName;
+    [SerializeField] public Animator noraTextBox;
+
     public float transitionTimer;
     private float count;
-    private float talkCount;
-    public float talkTimer;
+
+    public float transiitionTimer_;
+    private float count_;
+
+    private float noahTalkCount;
+    public float noahTalkTimer;
+
+    private float noraTalkCount;
+    public float noraTalkTimer;
  
 
-    private bool canTalk = false;
+    private bool noahCanTalk = false;
+    private bool noraCanTalk = false;
 
     private void Start()
     {
         fadeIn.SetTrigger("Start");
         count = transitionTimer;
-        talkCount = talkTimer;
+        count_ = transiitionTimer_;
+        noahTalkCount = noahTalkTimer;
+        noraTalkCount = noraTalkTimer;
     }
 
     private void Update()
     {
         NoahRise();
-        if (canTalk)
+        if (noahCanTalk)
         {
-            talkCount -= 1f * Time.deltaTime;
-            if (talkCount <= 0f) {talkCount = 0;}
-            if (talkCount == 0)
+            noahTalkCount -= 1f * Time.deltaTime;
+            if (noahTalkCount <= 0f) {noahTalkCount = 0;}
+            if (noahTalkCount == 0)
             {
                 noahTalking.SetBool("NoahTalk", true);
-                textBox.SetBool("TextOn", true);
+                noahTextBox.SetBool("TextOn", true);
+                noahName.SetBool("NoahIs", true);
+                NoraRise();
+            }
+        }
+
+        if (noraCanTalk)
+        {
+            noraTalkCount -= 1f * Time.deltaTime;
+            if(noraTalkCount <= 0f) {noraTalkCount = 0;}
+            if(noraTalkCount == 0)
+            {
+                noraTalking.SetBool("NoraTalk", true);
+                noraTextBox.SetBool("TextOn", true);
+                noraName.SetBool("NoraIs", true);
             }
         }
     }
@@ -45,7 +77,18 @@ public class EndCutScene : MonoBehaviour
         if (count == 0)
         {
             noahRise.SetBool("NoahOn",true);
-            canTalk = true;
+            noahCanTalk = true;
+        }
+    }
+
+    private void NoraRise()
+    {
+        count_ -= 1f * Time.deltaTime;
+        if (count_ <= 0f) { count_ = 0; }
+        if (count_ == 0)
+        {
+            noraRise.SetBool("NoraOn", true);
+            noraCanTalk = true;
         }
     }
 
